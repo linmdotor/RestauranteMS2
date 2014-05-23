de7eec75fd971ba973dac7dd898b52be81bc52ea
Amy15 <usuario_local@PTO1015>
2014-05-23 14:20:46 +0200

    SAPedido Ya no devuelve RespuestaCMD

--- RestauranteJPA/src/negocio/pedido/imp/SAPedidoImp.java
+++ RestauranteJPA/src/negocio/pedido/imp/SAPedidoImp.java
@@ -1,7 +1,6 @@
 package negocio.pedido.imp;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
@@ -14,34 +13,28 @@ import negocio.pedido.PedidoProducto;
 import negocio.pedido.SAPedido;
 import negocio.pedido.TPedido;
 import negocio.pedido.TPedidoProducto;
-import negocio.producto.Producto;
-import negocio.producto.ProductoNoPerecedero;
-import negocio.producto.ProductoPerecedero;
-import negocio.producto.TProducto;
-import negocio.producto.TProductoNoPerecedero;
-import negocio.producto.TProductoPerecedero;
-import negocio.producto.ValidarTProducto;
 import negocio.proveedor.Proveedor;
-import negocio.proveedor.SAProveedor;
 import negocio.proveedor.TProveedor;
 import negocio.proveedor.imp.SAProveedorImp;
 
-public class SAPedidoImp implements SAPedido{
+public class SAPedidoImp implements SAPedido {
 	protected EntityManager em;
-	
-	public TPedido obtenerPedido(int ID) throws Exception{
-		
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("UNIDAD_PERSISTENCIA_RESTAURANTE");
+
+	public TPedido obtenerPedido(int ID) throws Exception {
+
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("UNIDAD_PERSISTENCIA_RESTAURANTE");
 		EntityManager em = emf.createEntityManager();
-		
+
 		TPedido tPedido;
 		Pedido pedidoobtenido = null;
 		TypedQuery<Pedido> query = null;
-		
+
 		try {
 			em.getTransaction().begin();
 
-			query = em.createNamedQuery(Pedido.QUERY_OBTENER_PEDIDO, Pedido.class);
+			query = em.createNamedQuery(Pedido.QUERY_OBTENER_PEDIDO,
+					Pedido.class);
 
 			query.setParameter("arg", ID);
 
@@ -62,13 +55,13 @@ public class SAPedidoImp implements SAPedido{
 			if (ex instanceof Exception) {
 
 				em.getTransaction().rollback();
-				
+
 				throw ex;
 
 			} else {
 
 				em.getTransaction().rollback();
-				
+
 				throw new Exception(ex.getLocalizedMessage());
 
 			}
@@ -81,234 +74,219 @@ public class SAPedidoImp implements SAPedido{
 			em.close();
 
 		}
-		
-	
-		
+
 		tPedido = new TPedido(pedidoobtenido);
-		
+
 		return tPedido;
-		
-		
-		
+
 	}
-	
-	public List<TPedidoProducto> obtenerPedidoProductos (int IDPedido)
-	{
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("UNIDAD_PERSISTENCIA_RESTAURANTE");		
+
+	public List<TPedidoProducto> obtenerPedidoProductos(int IDPedido) {
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("UNIDAD_PERSISTENCIA_RESTAURANTE");
 		EntityManager em = emf.createEntityManager();
-		
+
 		@SuppressWarnings("rawtypes")
-		TypedQuery query = em.createQuery("SELECT e FROM PedidoProducto e", Pedido.class);
-		
+		TypedQuery query = em.createQuery("SELECT e FROM PedidoProducto e",
+				Pedido.class);
+
+		@SuppressWarnings("unchecked")
 		List<TPedidoProducto> listaPedido = query.getResultList();
-		
+
 		em.close();
 		emf.close();
 
 		return listaPedido;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public List<Pedido> obtenerPedidos(){
-	
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("UNIDAD_PERSISTENCIA_RESTAURANTE");		
+	public List<Pedido> obtenerPedidos() {
+
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("UNIDAD_PERSISTENCIA_RESTAURANTE");
 		EntityManager em = emf.createEntityManager();
-		
+
 		@SuppressWarnings("rawtypes")
-		TypedQuery query = em.createQuery("SELECT e FROM Pedido e", Pedido.class);
-		
+		TypedQuery query = em.createQuery("SELECT e FROM Pedido e",
+				Pedido.class);
+
 		List<Pedido> listaPedido = query.getResultList();
-		
+
 		em.close();
 		emf.close();
 
 		return listaPedido;
 	}
-	
-	
-	
-	public RespuestaCMD altaPedido(int IDprov)
-	{
-			
-		RespuestaCMD respuestaComando = new RespuestaCMD(EnumComandos.ERROR, "Error dando de alta un pedido.");
+
+	public boolean altaPedido(int IDprov) throws Exception {
+
+		boolean respuestaComando = false;;
 		Calendar fecha = new GregorianCalendar();
-		
-		if (IDprov >= 0){
-								
-			EntityManagerFactory emf = Persistence.createEntityManagerFactory("UNIDAD_PERSISTENCIA_RESTAURANTE");		
+
+		if (IDprov >= 0) {
+
+			EntityManagerFactory emf = Persistence
+					.createEntityManagerFactory("UNIDAD_PERSISTENCIA_RESTAURANTE");
 			EntityManager em = emf.createEntityManager();
-			
+
 			try {
-				
+
 				em.getTransaction().begin();
-			
-				//sacamos el pedido con id mayor
+
+				// sacamos el pedido con id mayor
 				List<Pedido> lista_pedidos = obtenerPedidos();
-				
+
 				int ultimo_id_usado = -1;
-				if(lista_pedidos != null)
-				for (int i=0; i<lista_pedidos.size(); i++)
-				{
-					if (lista_pedidos.get(i).getId_pedido() >= ultimo_id_usado)
-						ultimo_id_usado = lista_pedidos.get(i).getId_pedido();	
-				}
-				
-				//creamos un pedido con el último id usado + 1
+				if (lista_pedidos != null)
+					for (int i = 0; i < lista_pedidos.size(); i++) {
+						if (lista_pedidos.get(i).getId_pedido() >= ultimo_id_usado)
+							ultimo_id_usado = lista_pedidos.get(i)
+									.getId_pedido();
+					}
+
+				// creamos un pedido con el último id usado + 1
 				Pedido ped = new Pedido();
-				TProveedor tprov = new SAProveedorImp().obtenerProveedor(IDprov);
+				TProveedor tprov = new SAProveedorImp()
+						.obtenerProveedor(IDprov);
 				ped.setProveedor(new Proveedor(tprov));
 				ped.setFechaRealizado(fecha.toString());
 				ped.setId_pedido(ultimo_id_usado);
-				
+
 				em.persist(ped);
-				
-				em.getTransaction().commit();	
-				respuestaComando = new RespuestaCMD(EnumComandos.MODIFICAR_FORMULARIO_ALTA_PEDIDO, ultimo_id_usado);
-				
-			} 
-			catch(OptimisticLockException oe) {
-				respuestaComando = new RespuestaCMD(EnumComandos.ERROR, "Error al acceder los datos de forma concurrente.");
-			}			
-			catch (Exception e) {
-				
-				respuestaComando = new RespuestaCMD(EnumComandos.ERROR, "Error al crear pedido.");
-				
+
+				em.getTransaction().commit();
+				respuestaComando = true;
+
+			} catch (OptimisticLockException oe) {
+				throw new Exception("Error al acceder los datos de forma concurrente.");
+			} catch (Exception e) {
+				throw new Exception("Error al crear pedido.");
+
 			} finally {
-				 
+
 				em.close();
 				emf.close();
-				
-			 }		
-		
+
+			}
+
 		}
-		
+
 		return respuestaComando;
 
 	}
-	
-	
 
-	public RespuestaCMD cerrarPedido(List<Object> lista)
-	{
-		RespuestaCMD respuestaComando = new RespuestaCMD(EnumComandos.ERROR, "Error creando el pedido.");
+	@SuppressWarnings("unused")
+	public boolean cerrarPedido(List<TPedidoProducto> lista) throws Exception {
+		boolean respuestaComando = false;
 		Calendar fecha = new GregorianCalendar();
-		
+
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("UNIDAD_PERSISTENCIA_RESTAURANTE");
 		EntityManager em = emf.createEntityManager();
-		
-		//lo almacenamos Todo (el pedido y productos)
+
+		// lo almacenamos Todo (el pedido y productos)
 		em.getTransaction().begin();
-        try {
-           	
-        	for(int i=0; i<lista.size(); i++)
-        		em.persist((PedidoProducto)lista.get(i));      		
-        	
-            em.getTransaction().commit();
-            respuestaComando = new RespuestaCMD(EnumComandos.CORRECTO, "Exito al almacenar el Pedido.");
-            
-        } catch(OptimisticLockException oe) {
-			respuestaComando = new RespuestaCMD(EnumComandos.ERROR, "Error al acceder los datos de forma concurrente.");
-		}			
-		catch (Exception e) {
-            if (em.getTransaction().isActive()) {
-                try {
-                	
-                	respuestaComando = new RespuestaCMD(EnumComandos.ERROR, "Error en la trasacción..");
-                    em.getTransaction().rollback();
-                   
-	            } catch (RuntimeException re) {
-	                	respuestaComando = new RespuestaCMD(EnumComandos.ERROR, "Error al hacer rollback en la transacción.");
-	            }
-            }
-            respuestaComando = new RespuestaCMD(EnumComandos.ERROR, "Error en la trasacción..");
-        } finally {
-        	
-             em.close(); 
-        }
+		try {
+
+			for (int i = 0; i < lista.size(); i++)
+				em.persist(lista.get(i));
+
+			em.getTransaction().commit();
 			
-        return respuestaComando;
-	}
-	
-	
-	
-	
-	
-	public RespuestaCMD cancelarPedido(int ID){
-		
-		RespuestaCMD respuestaComando = new RespuestaCMD(EnumComandos.ERROR, "Error cancelando un pedido.");
-		Calendar fecha = new GregorianCalendar();		
-		
-		if (ID >= 0){
-			
-			EntityManagerFactory emf = Persistence.createEntityManagerFactory("UNIDAD_PERSISTENCIA_RESTAURANTE");
-			EntityManager em = emf.createEntityManager();
-			
-			try{
-				em.getTransaction().begin();
-				
-				Pedido pedido = em.find(Pedido.class, ID);
-				
-				if(pedido != null){
-					pedido.setFechaCancelado(fecha.getTime().toString()); 				
-					em.getTransaction().commit();
-					respuestaComando = new RespuestaCMD(EnumComandos.CORRECTO, "Exito cancelando un Pedido.");
+			respuestaComando = true;
+
+		} catch (OptimisticLockException oe) {
+			throw new Exception( "Error al acceder los datos de forma concurrente.");
+		} catch (Exception e) {
+			if (em.getTransaction().isActive()) {
+				try {
+					em.getTransaction().rollback();
+					throw new Exception("Error en la trasacción..");
+				} catch (RuntimeException re) {
+					throw new Exception("Error al hacer rollback en la transacción.");
 				}
-			} catch(OptimisticLockException oe) {
-				respuestaComando = new RespuestaCMD(EnumComandos.ERROR, "Error al acceder los datos de forma concurrente.");
-			}			
-			catch (Exception e) {
-				
-				respuestaComando = new RespuestaCMD(EnumComandos.ERROR, "Error al cancelar un pedido. Debe seleccionar un Pedido.");
-			} finally {
-				
-				em.close();
-				emf.close();
-				return respuestaComando;
 			}
-		} else
-			respuestaComando = new RespuestaCMD(EnumComandos.ERROR, "El ID debe ser positivo");
-	
+		} finally {
+
+			em.close();
+		}
+
 		return respuestaComando;
 	}
 
 
+	public boolean cancelarPedido(int ID) throws Exception {
 
-	public RespuestaCMD almacenarPedido(int ID){
-		
-		RespuestaCMD respuestaComando = new RespuestaCMD(EnumComandos.ERROR, "Error almacenando un pedido.");
-		Calendar fecha = new GregorianCalendar();		
-		
-		if (ID >= 0){
-			
-			EntityManagerFactory emf = Persistence.createEntityManagerFactory("UNIDAD_PERSISTENCIA_RESTAURANTE");
-			EntityManager em = emf.createEntityManager();
-			
-			try{
-				em.getTransaction().begin();
+		boolean respuestaComando = false;
+		Calendar fecha = new GregorianCalendar();
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("UNIDAD_PERSISTENCIA_RESTAURANTE");
+		EntityManager em = emf.createEntityManager();
+
+		try {
+			em.getTransaction().begin();
+
+			Pedido pedido = em.find(Pedido.class, ID);
+
+			if (pedido != null) {
 				
-				Pedido pedido = em.find(Pedido.class, ID);
-				
-				if(pedido != null){
-					pedido.setFechaEntregado(fecha.getTime().toString()); 				
-					em.getTransaction().commit();
-					respuestaComando = new RespuestaCMD(EnumComandos.CORRECTO, "Exito almacenando un Pedido.");
-				}
-			} catch(OptimisticLockException oe) {
-				respuestaComando = new RespuestaCMD(EnumComandos.ERROR, "Error al acceder los datos de forma concurrente.");
-			}			
-			catch (Exception e) {
-				
-				respuestaComando = new RespuestaCMD(EnumComandos.ERROR, "Error al almacenar un pedido. Debe seleccionar un Pedido.");
-			} finally {
-				
-				em.close();
-				emf.close();
-				return respuestaComando;
+				pedido.setFechaCancelado(fecha.getTime().toString());
+				em.getTransaction().commit();
+				respuestaComando = true;
+
 			}
-		} else
-			respuestaComando = new RespuestaCMD(EnumComandos.ERROR, "El ID debe ser positivo");
-	
+			
+		} catch (OptimisticLockException oe) {
+			throw new Exception(
+					"Error al acceder los datos de forma concurrente.");
+		} catch (Exception e) {
+			throw new Exception(
+					"Error al cancelar un pedido. Debe seleccionar un Pedido.");
+		} finally {
+			em.close();
+			emf.close();
+		}
+
 		return respuestaComando;
+	}
+
+	public boolean almacenarPedido(TPedido tpedido) throws Exception {
+
+		boolean respuesta = true;
+
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("UNIDAD_PERSISTENCIA_RESTAURANTE");
+		EntityManager em = emf.createEntityManager();
+
+		try {
+			em.getTransaction().begin();
+
+			// No deberia encontrar un pedido con el mismo ID
+			Pedido pedido = em.find(Pedido.class, tpedido.getId_pedido());
+
+			// Si lo encuentra no deberia almacenarlo
+			if (pedido == null) {
+				Pedido nuevoPedido = new Pedido(tpedido);
+
+				em.persist(nuevoPedido);
+
+				em.getTransaction().commit();
+
+				respuesta = true;
+
+			} else
+				respuesta = false;
+
+		} catch (OptimisticLockException oe) {
+			throw new Exception(
+					"No se almacenar el pedido, porque está bloqueado");
+		} catch (Exception e) {
+			throw new Exception("No se pudo almacenar el pedido.");
+		} finally {
+			em.close();
+			emf.close();
+		}
+
+		return respuesta;
 	}
 
 }
